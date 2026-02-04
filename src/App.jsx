cb0c55804c48994ea5ce526dba5ffe43ad44775c
Javohir1771 <gulomqodirovjavohir24@gmail.com>
2026-02-04 18:47:33 +0500

    login

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route, Routes } from 'react-router'
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path='/' />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const LoginForm = () => {
+  return (
+    <div className="w-96 bg-white p-8 rounded-xl shadow-lg flex flex-col gap-6">
+      <h2 className="text-2xl font-bold text-center mb-4">
+        Войти в Instagram
+      </h2>
+
+      <input
+        type="text"
+        placeholder="Логин"
+        className="border rounded-lg px-4 py-3 w-full text-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+      />
+      <input
+        type="password"
+        placeholder="Пароль"
+        className="border rounded-lg px-4 py-3 w-full text-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+      />
+
+      <button className="bg-blue-500 text-white py-3 rounded-lg w-full text-lg hover:bg-blue-600 font-semibold">
+        Войти
+      </button>
+
+      <a href="#" className="text-sm text-blue-600 text-center">
+        Забыли пароль?
+      </a>
+
+      <button className="bg-blue-700 text-white py-3 rounded-lg w-full text-lg hover:bg-blue-800 font-semibold">
+        Войти через Facebook
+      </button>
+
+      <a href="#" className="text-sm text-blue-600 text-center">
+        Создать новый аккаунт
+      </a>
+    </div>
+
+  );
+};
+
+export default LoginForm;
