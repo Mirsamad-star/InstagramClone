43d45b897b453b01e0a4c936c8667d61c95c7bbe
Javohir1771 <gulomqodirovjavohir24@gmail.com>
2026-02-04 18:52:47 +0500

    javohir login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Route, Routes } from 'react-router'
+import SignIn from './pages/register/SignIn'
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path='/' />
+        <Route path='/' element={<SignIn />} />
       </Routes>
     </>
   )
